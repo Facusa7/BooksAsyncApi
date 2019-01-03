ab6896632adba9d9bbedbf88aece25293ec6aef1
Facundo Sa <facundo.sa@web.com>
2019-01-03 17:04:52 -0300

    Books repository added

---
+++ Books.Api/Services/BooksRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Books.Api.Context;
+using Books.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Api.Services
+{
+    public class BooksRepository : IBooksRepository, IDisposable
+    {
+        private BooksContext _context;
+
+        public BooksRepository(BooksContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _context.Books.Include(b => b.Author).ToListAsync();
+        }
+
+        public async Task<Book> GetBookAsync(Guid id)
+        {
+            return await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this); //We say to the GC that this class was already cleaned up
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing) return;
+            if (_context == null) return;
+
+            _context.Dispose();
+            _context = null;
+        }
+    }
+}
